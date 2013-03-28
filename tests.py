6571734a2f751df3b5bc883737d7d97d93664823
David Pflug <david@tpflug.com>
2013-03-28 15:26:13 -0400

    Adds crystal, bot tests. Starts adding bear tests.
        Also, fixes a bug in a prior test.

--- tests.py
+++ tests.py
@@ -106,7 +106,7 @@ class TestGroup(TestBoard):
             self.assertEqual(self.game.get(5, 3), x + 12)
 
 
-class TestChain(unittest.TestCase):
+class TestChaining(unittest.TestCase):
     '''
     Test that chained matches are working correctly.
     '''
@@ -121,6 +121,10 @@ class TestChain(unittest.TestCase):
 
     def test_play_match_3_with_useless_neighbors(self):
         for x in xrange(3, 11):
+            self.game.current_item = 1
+            self.game.place(2, 2, None)
+            self.game.place(2, 3, 1)
+            self.game.place(3, 3, 1)
             self.game.place(2, 1, x)
             self.game.place(3, 2, x)
             self.game.place(1, 2, x)
@@ -178,6 +182,113 @@ class TestChain(unittest.TestCase):
         self.assertIsNone(self.game.get(2, 1))
         self.assertIsNone(self.game.get(3, 1))
         self.assertIsNone(self.game.get(3, 2))
+
+
+class TestImperialBot(unittest.TestCase):
+    '''
+    Test that the Imperial Bot kills bears, removes items,
+    penalizes destruction, and cannot be wasted.
+
+    However, the penalties are hand-assigned in the G+ and
+    Facebook versions of the game.
+    https://spryfox.zendesk.com/entries/22264993-What-are-the-point-penalties-for-botting-structures-
+    '''
+
+    def setUp(self):
+        self.game = TripleTown()
+        self.game.current_board = self.game.make_2d_board(6, 6)
+        self.game.score = 100000
+        self.game.current_item = 53
+
+    def test_bot_rock(self):
+        self.game.place(2, 3, 0)
+        self.game.play(2, 3)
+        self.assertIsNone(self.game.get(2, 3))
+        self.assertEqual(self.game.score, 100000)
+
+    def test_bot_grass(self):
+        self.game.place(2, 3, 1)
+        self.game.play(2, 3)
+        self.assertIsNone(self.game.get(2, 3))
+        self.assertEqual(self.game.score, 100000 - 10)
+
+    def test_bot_bear(self):
+        self.game.place(2, 3, 51)
+        self.game.play(2, 3)
+        self.assertEqual(self.game.get(2, 3), 50)
+        self.assertEqual(self.game.score, 100000)
+
+    def test_bot_ninja(self):
+        self.game.place(2, 3, 52)
+        self.game.play(2, 3)
+        self.assertEqual(self.game.get(2, 3), 50)
+        self.assertEqual(self.game.score, 100000)
+
+    def test_bot_blank(self):
+        self.assertFalse(self.game.play(2, 3))
+
+    def test_bot_castles(self):
+        for item in [7, 8, 18, 19]:
+            self.game.place(2, 3, item)
+            self.game.current_item = 53
+            self.assertFalse(self.game.play(2, 3))
+
+
+class TestCrystal(unittest.TestCase):
+    '''
+    Test that the crystal matches correctly, turns into a rock without match.
+    '''
+
+    def setUp(self):
+        self.game = TripleTown()
+        self.game.current_board = self.game.make_2d_board(6, 6)
+        self.game.score = 0
+        self.game.current_item = 0
+
+    def test_crystal_rock(self):
+        self.game.play(3, 3)
+        self.assertEqual(self.game.get(3, 3), 0)
+        self.assertEqual(self.game.score, 0)
+
+    def test_crystal_match_3(self):
+        for x in xrange(1, 8):
+            self.game.current_item = 0
+            self.game.place(3, 3, None)
+            self.game.place(2, 3, x)
+            self.game.place(3, 2, x)
+            self.game.play(3, 3)
+            self.assertEqual(self.game.get(3, 3), x + 1)
+
+    def test_crystal_match_4(self):
+        for x in xrange(1, 8):
+            self.game.current_item = 0
+            self.game.place(3, 3, None)
+            self.game.place(2, 3, x)
+            self.game.place(1, 3, x)
+            self.game.place(3, 2, x)
+            self.game.play(3, 3)
+            self.assertEqual(self.game.get(3, 3), x + 12)
+
+class TestBear(unittest.TestCase):
+    '''
+    Test that bears move when placed,
+    die when trapped,
+    don't move when trapped by other bears,
+    and their graves combine to churches at
+    the location of the most recent bear.
+    '''
+
+    def setUp(self):
+        self.game = TripleTown()
+        self.game.current_board = self.game.make_2d_board(6, 6)
+        self.game.score = 0
+        self.game.current_item = 51
+
+    def test_bear_move(self):
+        self.game.play(3, 3)
+        # They never choose to stay put.
+        self.assertIsNone(self.game.get(3, 3))
+
 
 if __name__ == '__main__':
     unittest.main()
